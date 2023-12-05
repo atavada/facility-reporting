8cf611b261d10854a2c3d09954293c7a6a336ecd
atavada <ardhaardhanaputra@gmail.com>
2023-12-06 05:45:49 +0700

    fix err dep

--- src/app/(admin)/dashboard/location/pagination.tsx
+++ src/app/(admin)/dashboard/location/pagination.tsx
@@ -7,13 +7,6 @@ import {
 import { Table } from "@tanstack/react-table";
 
 import { Button } from "@/components/ui/Button";
-// import {
-// 	Select,
-// 	SelectContent,
-// 	SelectItem,
-// 	SelectTrigger,
-// 	SelectValue,
-// } from "@/components/ui/Select";
 
 interface DataTablePaginationProps<TData> {
 	table: Table<TData>;
@@ -25,26 +18,6 @@ export function DataTablePagination<TData>({
 	return (
 		<div className='flex items-center justify-end'>
 			<div className='flex items-center space-x-6 lg:space-x-8'>
-				{/* <div className='flex items-center space-x-2'>
-					<p className='text-sm font-medium'>Rows per page</p>
-					<Select
-						value={`${table.getState().pagination.pageSize}`}
-						onValueChange={(value: any) => {
-							table.setPageSize(Number(value));
-						}}
-					>
-						<SelectTrigger className='h-8 w-[70px]'>
-							<SelectValue placeholder={table.getState().pagination.pageSize} />
-						</SelectTrigger>
-						<SelectContent side='top'>
-							{[10, 20, 30, 40, 50].map((pageSize) => (
-								<SelectItem key={pageSize} value={`${pageSize}`}>
-									{pageSize}
-								</SelectItem>
-							))}
-						</SelectContent>
-					</Select>
-				</div> */}
 				<div className='flex w-[100px] items-center justify-center text-sm font-medium'>
 					Page {table.getState().pagination.pageIndex + 1} of{" "}
 					{table.getPageCount()}
